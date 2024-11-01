1220807db1902b8e124ff48609f3f3d709a85b9e
Nathan B <emmanuel.n.bynum2024@outlook.com>
2024-11-01 18:51:58 -0500

    Created Register Page

--- src/routes/register/components/shared.ts
+++
@@ -1,6 +0,0 @@
-import { createFormContext } from '@mantine/form'
-import { RegisterForm } from '../types.ts'
-
-
-
-export const [RegisterFormProvider, useRegisterFormContext, useRegisterForm] = createFormContext<RegisterForm>()
--- src/utils/types/profile.ts
+++
@@ -1,6 +0,0 @@
-export type EnlistedType = 'PVT' | 'PV2' | 'PFC' | 'SPC' | 'CPL' | 'SGT' | 'SSG' | 'SFC' | '1SG'
-export type WarrantType = 'WO1' | 'CW2' | 'CW3' | 'CW4' | 'CW5'
-export type CommissionedType = '2LT' | '1LT' | 'CPT'
-export type RankType = EnlistedType | WarrantType | CommissionedType
-
-export type PlatoonType = 'Headquarters' | 'Maintenance' | 'Shops'
