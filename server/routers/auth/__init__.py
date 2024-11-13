d4c561588ec466e202a4d1f38898d09acd6797c2
Nathan B <emmanuel.n.bynum2024@outlook.com>
2024-11-13 12:59:03 -0600

    Further Refactor - Services and Server Routing

--- server/routers/auth/__init__.py
+++ server/routers/auth/__init__.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter
 
 from .authenticate import router as authenticate
-from .sign_in import router as sign_in
+from .authentication import router as sign_in
 from .register import router as register
 
 AuthRouter = APIRouter(prefix='/api/auth', tags=['auth'])
 AuthRouter.include_router(authenticate)
-AuthRouter.include_router(sign_in)
+AuthRouter.include_router(authentication)
 AuthRouter.include_router(register)
 
 __all__ = ['AuthRouter']
